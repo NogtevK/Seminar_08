4d2220f8826f8296dae6335ce3035331140ddd13
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-10-14 17:04:29 +0400

    task 53 complete/test OK/commit&push

--- Ex_01/Program.cs
+++ Ex_01/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 53: Задайте двумерный массив. Напишите программу,
+которая поменяет местами первую и последнюю строку
+массива.
+ */
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = 3, n = 4;
+int[,] mass = new int[m, n];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(0, 10);
+    }
+}
+Print(mass);
+Console.WriteLine();
+int element = 0;
+for (int j = 0; j < mass.GetLength(1); j++)
+{
+    element = mass[0, j];
+    mass[0, j] = mass[mass.GetLength(0) - 1, j];
+    mass[mass.GetLength(0) - 1, j] = element;
+
+    //Console.Write(mass[0, j] + " ");
+    //Console.Write(mass[mass.GetLength(0) - 1, j] + " ");
+}
+Console.WriteLine();
+Print(mass);
