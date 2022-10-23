c05103916b7bc15942f992823397fe2e929dccd3
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-10-23 22:14:31 +0400

    task 54 complete/testOK/commit/push

--- Homework_8/Program.cs
+++ Homework_8/Program.cs
@@ -1,7 +1,49 @@
 ﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая
 упорядочит по убыванию элементы каждой строки двумерного массива.
  */
- 
+
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] mass = new int[4, 4];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(0, 10);
+    }
+}
+Print(mass);
+Console.WriteLine();
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        for (int k = 0; k < mass.GetLength(1) - 1; k++)
+        {
+            if (mass[i, k] < mass[i, k + 1])
+
+            {
+                int temp = mass[i, k + 1];
+                mass[i, k + 1] = mass[i, k];
+                mass[i, k] = temp;
+            }
+        }
+    }
+}
+Print(mass);
+Console.WriteLine();
+
 /* Задача 56: Задайте прямоугольный двумерный массив. Напишите
 программу, которая будет находить строку с наименьшей суммой элементов.
  */
