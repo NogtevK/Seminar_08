b8862fd6032d2de838b149ebb70c1023216f8a5d
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-10-24 15:02:03 +0400

    task 56 complete/test ok/commit/push

--- Homework_8/Program.cs
+++ Homework_8/Program.cs
@@ -1,7 +1,7 @@
 ﻿/* Задача 54: Задайте двумерный массив. Напишите программу, которая
 упорядочит по убыванию элементы каждой строки двумерного массива.
  */
-
+/* 
 void Print(int[,] arr)
 {
     for (int i = 0; i < arr.GetLength(0); i++)
@@ -43,7 +43,52 @@ for (int i = 0; i < mass.GetLength(0); i++)
 }
 Print(mass);
 Console.WriteLine();
-
+ */
 /* Задача 56: Задайте прямоугольный двумерный массив. Напишите
 программу, которая будет находить строку с наименьшей суммой элементов.
  */
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] mass = new int[3, 4];
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        mass[i, j] = new Random().Next(0, 10);
+    }
+}
+Print(mass);
+Console.WriteLine();
+
+int minRowSum = int.MaxValue;
+int indexMinRow = 0;
+
+for (int i = 0; i < mass.GetLength(0); i++)
+{
+    int rowSum = 0;
+    for (int j = 0; j < mass.GetLength(1); j++)
+    {
+        rowSum += mass[i, j];
+    }
+    if (rowSum < minRowSum)
+    {
+        minRowSum = rowSum;
+        indexMinRow = i;
+    }
+}
+Console.WriteLine("Строка с минимальной суммой элементов");
+for (int j = 0; j < mass.GetLength(1); j++)
+{
+    Console.Write(mass[indexMinRow, j] + " ");
+}
+Console.WriteLine();
